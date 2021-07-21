48f77c34a8ca4cb79c2a49e4bd554826d9eb5dce
kasyanov_aa <kasyanov_aa@directum.ru>
2021-07-21 18:27:33 +0400

    Запросы на SQL [Rx]

--- GD.ReportsModule/GD.ReportsModule.ClientBase/Reports/ResultRequestReport/ResultRequestReportHandlers.cs
+++ GD.ReportsModule/GD.ReportsModule.ClientBase/Reports/ResultRequestReport/ResultRequestReportHandlers.cs
@@ -15,19 +15,23 @@ namespace GD.ReportsModule
       if (current == null || current.Department == null || current.Department.BusinessUnit == null)
       {
         e.Cancel = true;
+        return;
       }
       
       var dialog = Dialogs.CreateInputDialog(Resources.ReportParameters);
-      var currentBusinessUnit = current.Department.BusinessUnit;
+      var businessUnit = current.Department.BusinessUnit;
       var startDate = dialog.AddDate(Resources.StartDate, true, Calendar.BeginningOfYear(Calendar.Today));
       var endDate = dialog.AddDate(Resources.EndDate, true, Calendar.Today);
       
       if (dialog.Show() != DialogButtons.Ok)
+      {
         e.Cancel = true;
-      
+        return;
+      }
       ResultRequestReport.StartDate = startDate.Value.Value;
       ResultRequestReport.EndDate = endDate.Value.Value;
-      ResultRequestReport.BusinessUnitId = currentBusinessUnit.Id;
+      ResultRequestReport.BusinessUnitId = businessUnit.Id;      
+     // ResultRequestReport.RequestGuid = Constants.Module.RequestsDiscriminator.ToString();
     }
 
   }
--- GD.ReportsModule/GD.ReportsModule.Shared/ModuleConstants.cs
+++ GD.ReportsModule/GD.ReportsModule.Shared/ModuleConstants.cs
@@ -1,10 +1,11 @@
-using System;
+﻿using System;
 using Sungero.Core;
 
 namespace GD.ReportsModule.Constants
 {
   public static class Module
   {
-
+    // Дискриминатор "Обращения граждан".
+    public static readonly Guid RequestsDiscriminator = Guid.Parse("77300c65-4db1-4baf-9321-7e8efb7805cb");
   }
 }
